f21094b069cd34cff35782e6e1d5821205020023
Philipp Meier <p1meier@hsr.ch>
2013-09-26 08:45:12 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,17 @@ $(function() {
 
   'use strict';
 
-  var $navBar = $('nav');
+  $('abbr.timeago').timeago();
 
   // If touchscreen listen for touch, if not listen for click
   var hitEvent = 'ontouchstart' in document.documentElement ? 'touchstart' : 'click';
 
-  // Toggle the nav menu list when the user clicks the nav menu button
-  $('.menu').on(hitEvent, function () {
-    $navBar.toggleClass('nav-show');
+  $('.menu').on(hitEvent, function(event) {
+    event.preventDefault();
+    $('.nav-link').addClass('nav-show');
   });
 
-  $('.content').on(hitEvent, function(event) {
-    if ($navBar.hasClass('nav-show')) {
-      $navBar.removeClass('nav-show');
-    }
+  $('.content').on(hitEvent, function() {
+    $('.nav-link').removeClass('nav-show');
   });
 });
